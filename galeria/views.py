e17dfbcc3fb0b38b552c385a619b2ac97a80940d
Ricardo <ricardo.fa@gmail.com>
2024-08-23 15:06:58 -0300

    feat: Atualizar caminho do template index.html para tp_galeria

--- galeria/views.py
+++ galeria/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    return render(request, 'tp_galeria/index.html')
 
 # Create your views here.
