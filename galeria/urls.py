fad11dacd995683111cb569594f28eb70ef4938f
Ricardo <ricardo.fa@gmail.com>
2024-08-23 22:44:32 -0300

    Atualizar caminhos de templates e adicionar view para exibir imagem, aplicar DRY

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, view_imagem
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('imagem/', view_imagem, name='imagem'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'tp_galeria/index.html')
 
+
+def view_imagem(request):
+    return render(request, 'tp_galeria/imagem.html')
+
 # Create your views here.
